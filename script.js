6c8ffd68442009fc71892312302188957555d79e
Simon Pineda <simonpineda0521@gmail.com>
2022-10-14 14:06:19 -0500

    Cree lo que seran las bases de mi proyecto

---
+++ script.js
@@ -0,0 +1,6 @@
+// const cuadro = document.querySelector("div");
+
+// window.addEventListener("scroll", ()=>{
+//     let pix = window.pageYOffset
+//     cuadro.style = `left:${pix * 0.48}px;`;
+// });  -------------------aqui hice algo re raro, es para que un objeto cambie su posicion dependiendo de que tan abajo se encuentre-------------------
