07f15e1447aa5b5e13ec6b79955b5d9ab7638404
Simon Pineda <simonpineda0521@gmail.com>
2022-10-16 19:17:46 -0500

    Acabe de programar el funcionamiento de la calculadora

--- js/script.js
+++ js/script.js
@@ -1,10 +1,27 @@
 const wave1 = document.querySelector("#wave1");
 const wave2 = document.querySelector("#wave2");
-window.addEventListener("scroll", ()=>{
+window.addEventListener("scroll", () => {
     let pix = window.pageYOffset
-    let a = -860 + pix   
-    console.log(a)
+    let a = -860 + pix
     wave1.style = `transform:rotate(45deg) translate(${a}px, 0px);`;
     let b = 200 + pix
     wave2.style = `transform:rotate(-135deg) translate(${b}px, 0px);`;
 });
+const display = document.querySelector("#display");
+const calculatorDigit = document.querySelectorAll(".calculatorDigit");
+calculatorDigit.forEach((dig) => {
+    dig.addEventListener("click", () => {
+        if (dig.id === 'c') {
+            display.innerText = ""
+        }
+        else if (dig.id === 'back') {
+            display.innerText = display.innerText.slice(0, -1)
+        }
+        else if (dig.id === 'equal') {
+            display.innerText = eval(display.innerText)
+        }
+        else {
+            display.innerText += dig.innerText
+        }
+    })
+})
